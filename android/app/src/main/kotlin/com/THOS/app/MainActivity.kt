9764c6a31fd23e5dcc0d1e5eb921624122a9d152
Tallat Nadeem <tallat173@gmail.com>
2026-04-12 16:38:47 +0500

    new update

---
+++ android/app/src/main/kotlin/com/THOS/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.THOS.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/example/thos_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.thos_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
